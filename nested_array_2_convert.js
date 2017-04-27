81f8552ff5d461ff2812874891f30497b093b70a
raynormw <tirtawiryaputra@yahoo.com>
2017-04-27 16:51:05 +0700

    nested array 2 challenge done

--- nested_array_2_convert.js
+++ nested_array_2_convert.js
@@ -12,7 +12,18 @@ let roster = [
 // [[roster[0][0], roster[1][0]], [roster[0][1], roster[1][1]],...
 
 function convert_roster_format (nestedArray) {
-  // your convert code here
+  let arrayResult = [];
+
+  for (let i = 1; i < nestedArray.length; i++) {
+  	arrayResult.push({
+  		[nestedArray[0][0]] : nestedArray[i][0],
+  		[nestedArray[0][1]] : nestedArray[i][1],
+  		[nestedArray[0][2]] : nestedArray[i][2],
+  		[nestedArray[0][3]] : nestedArray[i][3]
+  	});
+  }
+
+  return arrayResult;
 }
 
 let object_roster = convert_roster_format(roster)
